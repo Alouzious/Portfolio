4c4f41f089724794906f14b17d3861714495a3ff
ALouzious <alouzious@gmail.com>
2025-07-30 15:23:26 +0300

    Resolve merge

--- Backend/portfolio_backend/settings.py
+++ Backend/portfolio_backend/settings.py
@@ -145,11 +145,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://portfolio-vez2.vercel.app"
+    "https://portfolio-vez2.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-
 ]
+
 
 
 
